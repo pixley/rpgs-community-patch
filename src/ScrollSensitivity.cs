bea73f5ecc6bba330a3ca484931623815eaa8bed
Tyler Pixley <tylerpixley@gmail.com>
2024-08-03 23:57:28 +0200

    scroll sensitivity increased #7

---
+++ src/ScrollSensitivity.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+using System.Reflection;
+
+namespace RpgsCommunityPatch
+{
+    [HarmonyPatch(typeof(LoopScrollRect), "OnScroll")]
+    public static class ScrollSensitivity
+    {
+        private static void Prepare(MethodBase original)
+        {
+            if (original != null)
+            {
+                Main.Log("Applied scroll sensitivity fix.");
+            }
+        }
+
+        private static void Prefix(LoopScrollRect __instance)
+        {
+            // By default, the Library's scroll sensitivity is 2, which is awful
+            // 40 was chosen based on feel
+            __instance.scrollSensitivity = 40f;
+        }
+    }
+}
