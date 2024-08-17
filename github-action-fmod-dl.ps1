28fd8970f230451ca82496f60e19c10419727fc4
Tyler Pixley <tylerpixley@gmail.com>
2024-08-17 23:44:07 +0200

    proper handling of envvar token

--- github-action-fmod-dl.ps1
+++ github-action-fmod-dl.ps1
@@ -7,12 +7,13 @@ if ($PSVersionTable.PSVersion.Major -lt 7)
 
 mkdir .\fmod_win32_mf\lib
 
+$auth_token = $Env:TOKEN
+
 $accept_raw_param = "application/vnd.github.raw+json"
 $accept_lfs_param = "application/vnd.git-lfs+json"
-$authorization_param = "Bearer {$TOKEN}"
+$authorization_param = "Bearer $auth_token"
 $content_type_param = "application/json"
 $version_param = "2022-11-28"
-
 $headers_one = @{"Accept"=$accept_raw_param;"X-GitHub-Api-Version"=$version_param;"Authorization"=$authorization_param}
 $response_one = Invoke-WebRequest -Headers $headers_one -Uri https://api.github.com/repos/pixley/fmod-lib-for-rpgs-patch/contents/fmod_vc.lib
 
