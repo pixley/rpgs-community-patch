547284929d295c411c50425c9cbd54a61812799b
Tyler Pixley <tylerpixley@gmail.com>
2024-08-18 00:13:33 +0200

    they just gave me the file in one go?

--- github-action-fmod-dl.ps1
+++ github-action-fmod-dl.ps1
@@ -24,25 +24,6 @@ if (-not $response_one.BaseResponse.IsSuccessStatusCode)
 	Exit 1
 }
 
-$response_one_str = [System.Text.Encoding]::UTF8.GetString($response_one.Content)
-$response_one_str = $response_one_str.Replace(' ', '=')
-
-Write-Output $response_one_str
-
-$response_one_dict = ConvertFrom-StringData -StringData $response_one_str
-
-$dl_request = @{operation="download";transfers=,"basic";objects=,@{oid=$response_one_dict["oid"].Substring(7);size=[int]$response_one_dict["size"]}}
-$dl_request_json = $dl_request | ConvertTo-Json -Depth 4
-
-$headers_two = @{"Accept"=$accept_lfs_param;"X-GitHub-Api-Version"=$version_param;"Content-Type"=$content_type_param;"Authorization"=$authorization_param}
-$response_two = Invoke-RestMethod -Method 'Post' -Headers $headers_two -Body $dl_request_json -Uri https://github.com/pixley/fmod-lib-for-rpgs-patch.git/info/lfs/objects/batch
-
-if (($response_two.objects.Count -eq 0) -or ($response_two.objects.Get(0).actions -eq $null))
-{
-	Write-Error "Did not receive valid download action."
-	Exit 1
-}
-
-Invoke-WebRequest -Uri $response_two.objects.Get(0).actions.download.href -OutFile .\fmod_win32_mf\lib\fmod_vc.dll
+$response_one.Content > .\fmod_win32_mf\lib\fmod_vc.lib
 
 Exit 0
