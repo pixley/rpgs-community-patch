67904db2c223505732a8f09cde3ff64d1cf219d6
Tyler Pixley <tylerpixley@gmail.com>
2024-08-18 11:07:13 +0200

    cleaned up vestigial stuff from multi-step approach, switched file writing to Set-Content

--- github-action-fmod-dl.ps1
+++ github-action-fmod-dl.ps1
@@ -1,3 +1,8 @@
+# ===================================
+# This script is specific to the GitHub Action that builds releases.
+# This has no use for manual development.
+# ===================================
+
 # PowerShell Core 7 or higher is required
 if ($PSVersionTable.PSVersion.Major -lt 7)
 {
@@ -5,25 +10,25 @@ if ($PSVersionTable.PSVersion.Major -lt 7)
     Exit 1
 }
 
+# Need to make the directory first
 mkdir .\fmod_win32_mf\lib
 
+# TOKEN envvar set via the Action YAML script
 $auth_token = $Env:TOKEN
 
 $accept_raw_param = "application/vnd.github.raw+json"
-$accept_lfs_param = "application/vnd.git-lfs+json"
 $authorization_param = "Bearer $auth_token"
-$content_type_param = "application/json"
 $version_param = "2022-11-28"
-$headers_one = @{"Accept"=$accept_raw_param;"X-GitHub-Api-Version"=$version_param;"Authorization"=$authorization_param}
-$response_one = Invoke-WebRequest -Headers $headers_one -Uri https://api.github.com/repos/pixley/fmod-lib-for-rpgs-patch/contents/fmod_vc.lib
+$headers = @{"Accept"=$accept_raw_param;"X-GitHub-Api-Version"=$version_param;"Authorization"=$authorization_param}
+$response = Invoke-WebRequest -Headers $headers -Uri https://api.github.com/repos/pixley/fmod-lib-for-rpgs-patch/contents/fmod_vc.lib
 
-if (-not $response_one.BaseResponse.IsSuccessStatusCode)
+if (-not $response.BaseResponse.IsSuccessStatusCode)
 {
-	$status_code = $response_one.BaseResponse.StatusCode
-	Write-Error "Could not get hash from GitHub.  Status code $status_code."
+	$status_code = $response.BaseResponse.StatusCode
+	Write-Error "Failure response from GitHub.  Status code $status_code."
 	Exit 1
 }
 
-$response_one.Content > .\fmod_win32_mf\lib\fmod_vc.lib
+Set-Content .\fmod_win32_mf\lib\fmod_vc.lib -Value $response.Content -Encoding Byte
 
 Exit 0
